f06b8c0b280477bfc74a152151fa8af9a790739d
Raphael Lira <12634847785@cefet-rj.br>
2025-01-31 17:38:23 -0300

    feito

--- trabalho.sh
+++ trabalho.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+sudo apt install -y fortune
+
+fortune > mensagem1.txt
+fortune > mensagem2.txt
+fortune > mensagem3.txt
+cat mensagem1.txt
+cat mensagem2.txt
+cat mensagem3.txt
+
+cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
+cat mensagens.txt
+
+cp mensagens.txt mensagens_antiga.txt
+fortune >> mensagens.txt
+
+wc -l mensagens.txt > linhas.txt
+
+egrep -o 'a' mensagens.txt | wc -l > letras.txt
+
+ls -R /etc > saida.txt 2> erros.txt
+
+ls -R /etc &> saida_com_erros.txt
+
+cut -d: -f1 /etc/passwd > usuarios.txt
+
+cut -d: -f1 /etc/passwd | sort > usuarios_ordenados.txt
